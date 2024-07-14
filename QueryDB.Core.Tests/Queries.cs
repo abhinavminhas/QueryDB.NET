907ac3637582f42034c9405cdb9fb964702fcc09
abhinavminhas <abhinavminhas@users.noreply.github.com>
2024-07-14 13:58:57 +1000

    Test updates

--- QueryDB.Core.Tests/Queries.cs
+++ QueryDB.Core.Tests/Queries.cs
@@ -4,17 +4,17 @@
     {
         internal static class OracleQuery
         {
-            public static string SelectSql = @"select SYS_CONTEXT('USERENV', 'DB_NAME') AS current_database FROM dual";
+            public static string SelectSql = @"SELECT name AS current_database FROM v$database";
         }
 
         internal static class SqlServerQuery
         {
-            public static string SelectSql = @"select DB_NAME() AS current_database";
+            public static string SelectSql = @"SELECT DB_NAME() AS current_database";
         }
 
         internal static class MySqlQuery
         {
-            public static string SelectSql = @"select DATABASE() AS current_database";
+            public static string SelectSql = @"SELECT DATABASE() AS current_database";
         }
     }
 }
