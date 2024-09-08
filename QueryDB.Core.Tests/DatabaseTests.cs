843d47cea90dcde265bd237223b316c4195f6758
abhinavminhas <abhinavminhas@users.noreply.github.com>
2024-09-08 19:56:30 +1000

    Including tests

--- QueryDB.Core.Tests/DatabaseTests.cs
+++ QueryDB.Core.Tests/DatabaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace QueryDB.Core.Tests
 {
@@ -6,32 +7,13 @@ namespace QueryDB.Core.Tests
     public class DatabaseTests : TestBase
     {
 
-        #region Oracle DB Tests
-
-        [TestMethod]
-        [TestCategory(DB_TESTS), TestCategory(ORACLE_TESTS), TestCategory(SMOKE_TESTS)]
-        public void Test_Oracle_FetchData()
-        {
-            var selectSql = Queries.OracleQuery.SelectSql;
-            var data = new DBContext(DB.Oracle, OracleDatabaseString).FetchData(selectSql);
-            Assert.IsTrue(data.Count > 0);
-            Assert.AreEqual("oracle", data[0].ReferenceData["CURRENT_DATABASE"]);
-
-            var dbContext = new DBContext(DB.Oracle, OracleDatabaseString);
-            data = dbContext.FetchData(selectSql);
-            Assert.IsTrue(data.Count > 0);
-            Assert.AreEqual("oracle", data[0].ReferenceData["CURRENT_DATABASE"]);
-        }
-
-        #endregion
-
         #region MSSQL DB Tests
 
         [TestMethod]
         [TestCategory(DB_TESTS), TestCategory(MSSQL_TESTS), TestCategory(SMOKE_TESTS)]
         public void Test_MSSQL_FetchData()
         {
-            var selectSql = Queries.SqlServerQuery.SelectSql;
+            var selectSql = Queries.SQLServerQueries.Smoke.SelectSql;
             var data = new DBContext(DB.MSSQL, SqlServerDatabaseString).FetchData(selectSql);
             Assert.IsTrue(data.Count > 0);
             Assert.AreEqual("mssql", data[0].ReferenceData["current_database"]);
@@ -42,6 +24,38 @@ namespace QueryDB.Core.Tests
             Assert.AreEqual("mssql", data[0].ReferenceData["current_database"]);
         }
 
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(MSSQL_TESTS)]
+        public void Test_MSSQL_FetchData_SelectQuery_LowerCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.MSSQL, SqlServerDatabaseString).FetchData(selectSql);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["Agent_Name"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["Agent_Code"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["Agent_Name"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["Working_Area"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["Commission"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["Phone_No"]);
+            Assert.AreEqual("", agent.ReferenceData["Country"]);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(MSSQL_TESTS)]
+        public void Test_MSSQL_FetchData_SelectQuery_UpperCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.MSSQL, SqlServerDatabaseString).FetchData(selectSql, upperCaseKeys: true);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["AGENT_NAME"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["AGENT_CODE"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["AGENT_NAME"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["WORKING_AREA"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["COMMISSION"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["PHONE_NO"]);
+            Assert.AreEqual("", agent.ReferenceData["COUNTRY"]);
+        }
+
         #endregion
 
         #region MySQL DB Tests
@@ -50,7 +64,7 @@ namespace QueryDB.Core.Tests
         [TestCategory(DB_TESTS), TestCategory(MYSQL_TESTS), TestCategory(SMOKE_TESTS)]
         public void Test_MySQL_FetchData()
         {
-            var selectSql = Queries.MySqlQuery.SelectSql;
+            var selectSql = Queries.MySQLQueries.Smoke.SelectSql;
             var data = new DBContext(DB.MySQL, MySqlDatabaseString).FetchData(selectSql);
             Assert.IsTrue(data.Count > 0);
             Assert.AreEqual("mysql", data[0].ReferenceData["current_database"]);
@@ -59,6 +73,89 @@ namespace QueryDB.Core.Tests
             data = dbContext.FetchData(selectSql);
             Assert.IsTrue(data.Count > 0);
             Assert.AreEqual("mysql", data[0].ReferenceData["current_database"]);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(MYSQL_TESTS)]
+        public void Test_MySQL_FetchData_SelectQuery_LowerCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.MySQL, SqlServerDatabaseString).FetchData(selectSql);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["Agent_Name"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["Agent_Code"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["Agent_Name"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["Working_Area"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["Commission"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["Phone_No"]);
+            Assert.AreEqual("", agent.ReferenceData["Country"]);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(MYSQL_TESTS)]
+        public void Test_MySQL_FetchData_SelectQuery_UpperCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.MySQL, SqlServerDatabaseString).FetchData(selectSql, upperCaseKeys: true);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["AGENT_NAME"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["AGENT_CODE"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["AGENT_NAME"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["WORKING_AREA"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["COMMISSION"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["PHONE_NO"]);
+            Assert.AreEqual("", agent.ReferenceData["COUNTRY"]);
+        }
+
+        #endregion
+
+        #region Oracle DB Tests
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(ORACLE_TESTS), TestCategory(SMOKE_TESTS)]
+        public void Test_Oracle_FetchData()
+        {
+            var selectSql = Queries.OracleQueries.Smoke.SelectSql;
+            var data = new DBContext(DB.Oracle, OracleDatabaseString).FetchData(selectSql);
+            Assert.IsTrue(data.Count > 0);
+            Assert.AreEqual("oracle", data[0].ReferenceData["CURRENT_DATABASE"]);
+
+            var dbContext = new DBContext(DB.Oracle, OracleDatabaseString);
+            data = dbContext.FetchData(selectSql);
+            Assert.IsTrue(data.Count > 0);
+            Assert.AreEqual("oracle", data[0].ReferenceData["CURRENT_DATABASE"]);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(ORACLE_TESTS)]
+        public void Test_Oracle_FetchData_SelectQuery_LowerCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.Oracle, SqlServerDatabaseString).FetchData(selectSql);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["Agent_Name"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["Agent_Code"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["Agent_Name"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["Working_Area"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["Commission"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["Phone_No"]);
+            Assert.AreEqual("", agent.ReferenceData["Country"]);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(ORACLE_TESTS)]
+        public void Test_Oracle_FetchData_SelectQuery_UpperCaseKeys()
+        {
+            var selectSql = Queries.SQLServerQueries.SalesDB.SelectSql;
+            var data = new DBContext(DB.Oracle, SqlServerDatabaseString).FetchData(selectSql, upperCaseKeys: true);
+            Assert.IsTrue(data.Count == 12);
+            var agent = data.FirstOrDefault(X => X.ReferenceData["AGENT_NAME"] == "Benjamin");
+            Assert.AreEqual("A009", agent.ReferenceData["AGENT_CODE"]);
+            Assert.AreEqual("Benjamin", agent.ReferenceData["AGENT_NAME"]);
+            Assert.AreEqual("Hampshair", agent.ReferenceData["WORKING_AREA"]);
+            Assert.AreEqual("0.11", agent.ReferenceData["COMMISSION"]);
+            Assert.AreEqual("008-22536178", agent.ReferenceData["PHONE_NO"]);
+            Assert.AreEqual("", agent.ReferenceData["COUNTRY"]);
         }
 
         #endregion
--- QueryDB.Core.Tests/Queries.cs
+++ QueryDB.Core.Tests/Queries.cs
@@ -2,19 +2,40 @@
 {
     internal static class Queries
     {
-        internal static class OracleQuery
+        internal static class OracleQueries
         {
-            public static string SelectSql = @"SELECT 'oracle' AS current_database FROM dual";
+            internal static class Smoke
+            {
+                public static string SelectSql = @"SELECT 'oracle' AS current_database FROM dual";
+            }
+            internal static class SalesDB
+            {
+                public static string SelectSql = @"SELECT * FROM AGENTS";
+            }
         }
 
-        internal static class SqlServerQuery
+        internal static class SQLServerQueries
         {
-            public static string SelectSql = @"SELECT 'mssql' AS current_database";
+            internal static class Smoke
+            {
+                public static string SelectSql = @"SELECT 'mssql' AS current_database";
+            }
+            internal static class SalesDB
+            {
+                public static string SelectSql = @"SELECT * FROM AGENTS";
+            }
         }
 
-        internal static class MySqlQuery
+        internal static class MySQLQueries
         {
-            public static string SelectSql = @"SELECT DATABASE() AS current_database";
+            internal static class Smoke
+            {
+                public static string SelectSql = @"SELECT DATABASE() AS current_database";
+            }
+            internal static class SalesDB
+            {
+                public static string SelectSql = @"SELECT * FROM AGENTS";
+            }
         }
     }
 }
