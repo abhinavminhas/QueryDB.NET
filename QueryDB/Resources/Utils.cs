73a09e79b38c0c7ce4231c6be928139e1d6c0eb8
abhinavminhas <abhinavminhas@users.noreply.github.com>
2024-10-27 13:26:04 +1100

    Utils - Documentation

--- QueryDB/Resources/Utils.cs
+++ QueryDB/Resources/Utils.cs
@@ -4,8 +4,17 @@ using System.Data;
 
 namespace QueryDB.Resources
 {
+    /// <summary>
+    /// Provides a set of utility functions for various operations.
+    /// </summary>
     internal static class Utils
     {
+        /// <summary>
+        /// Checks if a specified column exists in the given data reader.
+        /// </summary>
+        /// <param name="reader">The data reader to check.</param>
+        /// <param name="columnName">The name of the column to find.</param>
+        /// <returns>Returns <c>true</c> if the column exists; otherwise, <c>false</c>.</returns>
         internal static bool ColumnExists(IDataReader reader, string columnName)
         {
             try
@@ -19,12 +28,26 @@ namespace QueryDB.Resources
             }
         }
 
+        /// <summary>
+        /// Determines if a specified column in an Oracle data reader is of type "BFILE".
+        /// </summary>
+        /// <param name="reader">The Oracle data reader containing the column.</param>
+        /// <param name="columnIndex">The index of the column to check.</param>
+        /// <returns>Returns <c>true</c> if the column is of type "BFILE"; otherwise, <c>false</c>.</returns>
+
         internal static bool IsBFileColumn(OracleDataReader reader, int columnIndex)
         {
             const string BFILE_COLUMN = "BFILE";
             string columnType = reader.GetDataTypeName(columnIndex);
             return columnType.Equals(BFILE_COLUMN, StringComparison.OrdinalIgnoreCase);
         }
+
+        /// <summary>
+        /// Retrieves the content of a BFILE column from an Oracle data reader as a Base64-encoded string.
+        /// </summary>
+        /// <param name="reader">The Oracle data reader containing the BFILE column.</param>
+        /// <param name="columnIndex">The index of the BFILE column to read.</param>
+        /// <returns>Returns the BFILE content as a Base64-encoded string, or an empty string if the BFILE is null.</returns>
 
         internal static string GetBFileContent(OracleDataReader reader, int columnIndex)
         {
