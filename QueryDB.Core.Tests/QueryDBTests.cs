7d16e5176e14d00e09e1907aa6801eb14b29ccf0
abhinavminhas <abhinavminhas@users.noreply.github.com>
2025-02-24 00:48:02 +1100

    Test - Execute Command Async

--- QueryDB.Core.Tests/QueryDBTests.cs
+++ QueryDB.Core.Tests/QueryDBTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace QueryDB.Core.Tests
 {
@@ -17,6 +18,18 @@ namespace QueryDB.Core.Tests
 
             var dbContext = new DBContext((DB)999, "some_invalid_connection_string");
             var result = dbContext.ExecuteCommand(sqlStatement);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        [TestCategory(DB_TESTS), TestCategory(UNKNOW_DB_TESTS)]
+        public async Task ExecuteCommandAsync_UnknownDB_ReturnsNegativeOne()
+        {
+            string sqlStatement = "DELETE FROM users";
+
+            var dbContext = new DBContext((DB)999, "some_invalid_connection_string");
+            var result = await dbContext.ExecuteCommandAsync(sqlStatement);
 
             Assert.AreEqual(-1, result);
         }
